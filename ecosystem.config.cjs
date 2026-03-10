38d8fac51fb062e43f2d6f1bf4bf26545ce8ae86
root <root@viveros.click>
2026-03-10 15:02:07 +0100

    cambiso en el archivo

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,14 +7,13 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '500M',
-    env: {
-      NODE_ENV: 'development',
-      PORT: 5000
-    },
     env_production: {
-      NODE_ENV: 'production',
-      PORT: 5000
-    },
+    NODE_ENV: 'production',
+    PORT: 5000,
+    DATABASE_URL: 'postgresql://salud_digital:Lola5109@localhost:5432/salud_digital',
+    SESSION_SECRET: '970182f8d80017c01fbed37468b67b80ee0eccb577c4faa9a05f223c0e799457',
+    ADMIN_PASSWORD: 'AdminSeguro2026!'
+},
     // Logs
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
     error_file: '/var/log/pm2/salud-digital-error.log',
